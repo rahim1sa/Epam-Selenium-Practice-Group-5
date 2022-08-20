32086c47483c27b68e27e4409a7e2a3a8d7a675d
Rahim Isa <rahim.isa@outlook.com>
2022-08-21 00:36:18 +0200

    Changes after comment

--- src/test/java/org/example/VerifyFilterAndSearchTest.java
+++ src/test/java/org/example/VerifyFilterAndSearchTest.java
@@ -7,23 +7,38 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 import java.time.Duration;
 import java.util.List;
 
 public class VerifyFilterAndSearchTest {
+    private WebDriver webDriver;
+    private WebDriverWait webDriverWait;
+    int min=0, max=0, price=0, wholePrice=0;
 
-    @Test
-    public void verifyBrandName(){
+    @BeforeMethod
+    public void setup(){
         System.setProperty(
                 "webdriver.chrome.driver",
                 "C:\\Users\\Asus\\Desktop\\Epam\\First_Selenium\\src\\test\\resources\\webdriver\\chromedriver.exe"
         );
-        WebDriver webDriver = new ChromeDriver();
+        webDriver = new ChromeDriver();
         webDriver.get("https://www.amazon.com");
         webDriver.manage().window().maximize();
+        webDriverWait = new WebDriverWait(webDriver, Duration.ofSeconds(5));
+    }
 
+    @AfterMethod
+    public void quite(){
+        webDriver.close();
+        webDriver.quit();
+    }
+
+    @Test
+    public void verifyBrandName(){
         // redirecting category page
         webDriver.findElement(By.xpath("//*[contains(@aria-label,\"Computers & Accessories\")]")).click();
 
@@ -31,74 +46,70 @@ public class VerifyFilterAndSearchTest {
         webDriver.findElement(By.xpath("//*[@id=\"s-refinements\"]/div[5]/ul/li[1]/span/a")).click();
 
         // taking category name
-        WebElement titleApple = new WebDriverWait(webDriver, Duration.ofSeconds(5)).
-                until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@id=\"p_89/Apple\"]/span/a/span")));
+        WebElement titleApple =webDriverWait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@id=\"p_89/Apple\"]/span/a/span")));
 
         // checking filtered item names whether it's matched with category name
         List<WebElement> resultList = webDriver.findElements(By.xpath("//*[contains(@class,\"a-size-base-plus a-color-base a-text-normal\")]"));
         for (WebElement x: resultList){
             Assert.assertTrue(x.getText().contains(titleApple.getText()));
         }
-
     }
 
+
+    public int findMin(String priceRange){
+        min = Integer.parseInt(priceRange.substring(1,priceRange.indexOf(" ")));
+        return min;
+    }
+    public int findMax(String priceRange){
+        max = Integer.parseInt(priceRange.substring(priceRange.lastIndexOf("$") + 1));
+        return max;
+    }
     @Test
     public void verifyPriceRange(){
-        System.setProperty(
-                "webdriver.chrome.driver",
-                "C:\\Users\\Asus\\Desktop\\Epam\\First_Selenium\\src\\test\\resources\\webdriver\\chromedriver.exe"
-        );
-        WebDriver webDriver = new ChromeDriver();
-        webDriver.get("https://www.amazon.com");
-        webDriver.manage().window().maximize();
 
         // redirecting category page
         webDriver.findElement(By.xpath("//*[contains(@aria-label,\"Computers & Accessories\")]")).click();
 
-        // taking price range
-        WebElement priceRange = webDriver.findElement(By.xpath("//*[@id=\"s-refinements\"]/div[12]/ul/li[3]/span/a"));
-        int max = Integer.parseInt(priceRange.getText().substring(priceRange.getText().length() - 3));
-        int min = Integer.parseInt(priceRange.getText().substring(1, 3));
+        int priceRangeCount = webDriver.findElements(By.xpath("//*[@id=\"s-refinements\"]/div[12]/ul/li")).size();
 
-        // filter by price range
-        webDriver.findElement(By.xpath("//*[@id=\"s-refinements\"]/div[12]/ul/li[3]/span/a/span")).click();
+        for (int i=1;i <= priceRangeCount;i++){
+            // taking price range
+            String priceRange = webDriver.findElement(By.xpath("//*[@id=\"s-refinements\"]/div[12]/ul/li["+ i +"]/span/a")).getText();
 
-        // checking price range
-        List<WebElement> resultList = webDriver.findElements(By.xpath("//*[contains(@class,\"s-result-list\")]//*[contains(@class,\"a-price-whole\")]"));
-        for (int i=0;i<resultList.size();i++){
+            // filter by price range
+            webDriverWait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@id=\"s-refinements\"]/div[12]/ul/li["+ i +"]/span/a/span"))).click();
 
-            int wholePrice = Integer.parseInt(resultList.get(i).getText());
-            if (wholePrice > min && wholePrice < max){
-                System.out.println("Product-"+i+" price(" + wholePrice + ") is between "+min+" and "+max);
-            }else {
-                System.out.println("Product-"+i+" price(" + wholePrice + ") isn't between "+min+" and "+max);
+            // checking price range
+            List<WebElement> resultList = webDriver.findElements(By.xpath("//*[contains(@class,\"s-result-list\")]//*[contains(@class,\"a-price-whole\")]"));
+            for (int j=0;j<resultList.size();j++){
+                if (i==1 || i==priceRangeCount)  {
+                    price = Integer.parseInt(priceRange.replaceAll("[^\\d.]", ""));
+                }
+                wholePrice = Integer.parseInt(resultList.get(j).getText());
+                if (priceRange.contains("Under")){
+                    Assert.assertTrue(wholePrice > price,"Product-"+j+" price(" + wholePrice + ") is under "+price);
+                }else if(priceRange.contains("Above")) {
+                    Assert.assertTrue(wholePrice > price, "Product-"+j+" price(" + wholePrice + ") is under "+price);
+                }else {
+                    Assert.assertTrue(wholePrice > findMin(priceRange) && wholePrice < findMax(priceRange)
+                            ,"Product-"+j+" price(" + wholePrice + ") is not between "+findMin(priceRange)+" and "+findMax(priceRange));
+                }
             }
+            webDriver.navigate().back();
         }
 
     }
 
     @Test
     public void verifySortedPrice(){
-        System.setProperty(
-                "webdriver.chrome.driver",
-                "C:\\Users\\Asus\\Desktop\\Epam\\First_Selenium\\src\\test\\resources\\webdriver\\chromedriver.exe"
-        );
-        WebDriver webDriver = new ChromeDriver();
-        webDriver.get("https://www.amazon.com");
-        webDriver.manage().window().maximize();
 
         // redirecting category page
         webDriver.findElement(By.xpath("//*[contains(@aria-label,\"Computers & Accessories\")]")).click();
-
         // filter by price range
-        new WebDriverWait(webDriver,Duration.ofSeconds(3)).
-                until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@id=\"s-refinements\"]/div[12]/ul/li[3]/span/a/span"))).click();
-
+        webDriverWait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@id=\"s-refinements\"]/div[12]/ul/li[3]/span/a/span"))).click();
         // sort from "low" to "high"
         webDriver.findElement(By.xpath("//*[@id=\"a-autoid-0\"]/span")).click();
         webDriver.findElement(By.id("s-result-sort-select_1")).click();
-
-
         // verify sorted price
         List<WebElement> resultList = webDriver.findElements(By.xpath("//*[contains(@class,\"s-result-list\")]//*[contains(@class,\"a-price-whole\")]"));
 
@@ -106,12 +117,7 @@ public class VerifyFilterAndSearchTest {
             int currentPrice = Integer.parseInt(resultList.get(i).getText());
             int nextPrice = Integer.parseInt(resultList.get(i+1).getText());
 
-            if (currentPrice <= nextPrice ){
-                System.out.println("ordered price-"+resultList.get(i+1).getText());
-            }else {
-                System.out.println("unordered price-"+resultList.get(i+1).getText());
-            }
+            Assert.assertTrue(currentPrice <= nextPrice,"unordered price-"+resultList.get(i+1).getText() );
         }
-
     }
 }
